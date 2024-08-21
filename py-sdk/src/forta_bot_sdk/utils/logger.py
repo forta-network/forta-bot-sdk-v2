dcc46f114358f06d492646ba151c8ecd50b087c7
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2024-08-21 19:59:03 +0400

    fix logger initialization

--- py-sdk/src/forta_bot_sdk/utils/logger.py
+++ py-sdk/src/forta_bot_sdk/utils/logger.py
@@ -4,7 +4,7 @@ from .now import now
 
 
 class Logger:
-    def __init__(self, is_prod: bool, is_debug: bool, is_logging_disabled: bool):
+    def __init__(self, is_prod: bool = False, is_debug: bool = False, is_logging_disabled: bool = False):
         # TODO make use of built-in python logging module
         self.is_prod = is_prod
         self.is_debug = is_debug
