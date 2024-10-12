eb08ebdf6a0ee774ea402dc35b1a04b0f3812e65
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2024-10-12 09:47:30 +0400

    improve snake to camel case util function

--- py-sdk/src/forta_bot_sdk/utils/snake_to_camel_case.py
+++ py-sdk/src/forta_bot_sdk/utils/snake_to_camel_case.py
@@ -1,17 +1,4 @@
 
 def snake_to_camel_case(val: str) -> str:
-    if len(val) == 0 or "_" not in val:
-        return val
-
-    new_val = []
-    should_capitalize = False
-    for char in val:
-        if char == "_":
-            should_capitalize = True
-            continue
-        elif should_capitalize:
-            new_val.append(char.capitalize())
-            should_capitalize = False
-        else:
-            new_val.append(char)
-    return ''.join(new_val)
+    camel_case = "".join(x.capitalize() for x in val.lower().split("_"))
+    return val[0].lower() + camel_case[1:]
