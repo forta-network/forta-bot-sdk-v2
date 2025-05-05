494339d4721f1ee660509fe0cc2cf813ec7d7ab8
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2025-05-05 13:11:46 +0400

    export now function; use no-op cache

--- py-sdk/src/forta_bot_sdk/__init__.py
+++ py-sdk/src/forta_bot_sdk/__init__.py
@@ -11,7 +11,7 @@ from .health import RunHealthCheck
 from .jwt import MOCK_JWT, DecodeJwt, GetScannerJwt, VerifyJwt
 from .attester import RunAttester
 from .common import AttestTransaction, AttestTransactionResult
-from .utils import BloomFilter, keccak256, snake_to_camel_case, GetBotId, GetChainId, GetBotOwner, GetFortaChainId, get_create_address, logger
+from .utils import BloomFilter, keccak256, snake_to_camel_case, GetBotId, GetChainId, GetBotOwner, GetFortaChainId, get_create_address, logger, now
 from .di import RootContainer
 
 container = RootContainer()
--- py-sdk/src/forta_bot_sdk/cache/di.py
+++ py-sdk/src/forta_bot_sdk/cache/di.py
@@ -3,6 +3,7 @@ import pickledb
 from dependency_injector import containers, providers
 from .get_json_rpc_cache_provider import provide_get_json_rpc_cache_provider
 from .is_cache_healthy import provide_is_cache_healthy
+from .cache import Cache
 from .disk_cache import DiskCache
 from .json_rpc_cache import JsonRpcCache
 
@@ -11,7 +12,9 @@ class CacheContainer(containers.DeclarativeContainer):
     common = providers.DependenciesContainer()
     metrics = providers.DependenciesContainer()
 
-    def provide_cache(is_prod: bool, disk_cache: DiskCache, json_rpc_cache: JsonRpcCache):
+    def provide_cache(is_prod: bool, disk_cache: DiskCache, json_rpc_cache: JsonRpcCache, no_op_cache: Cache):
+        if "FORTA_CLI_NO_CACHE" in os.environ:
+            return no_op_cache
         return json_rpc_cache if is_prod else disk_cache
 
     def provide_json_rpc_cache_url():
@@ -49,7 +52,9 @@ class CacheContainer(containers.DeclarativeContainer):
         metrics_helper=metrics.metrics_helper,
         with_retry=common.with_retry,
         logger=common.logger)
+    no_op_cache = providers.Singleton(Cache)
     cache = providers.Callable(provide_cache,
                                is_prod=common.is_prod,
                                disk_cache=disk_cache,
-                               json_rpc_cache=json_rpc_cache)
+                               json_rpc_cache=json_rpc_cache,
+                               no_op_cache=no_op_cache)
