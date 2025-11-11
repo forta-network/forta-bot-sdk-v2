a746b750cb4afd81489008c72a77fbadaa41a3e1
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2025-11-11 15:39:53 +0400

    add support for 7702 tx authorization list

--- py-sdk/src/forta_bot_sdk/transactions/transaction.py
+++ py-sdk/src/forta_bot_sdk/transactions/transaction.py
@@ -2,6 +2,17 @@ from typing import Optional
 from ..utils import hex_to_int, get_dict_val, format_address, JSONable
 
 
+class Authorization(JSONable):
+    def __init__(self, dict):
+        self.chain_id: int = hex_to_int(
+            get_dict_val(dict, 'chainId', 'chain_id'))
+        self.address: str = format_address(dict.get('address'))
+        self.nonce: int = hex_to_int(dict.get('nonce'))
+        self.y_parity: str = get_dict_val(dict, 'yParity', 'y_parity')
+        self.r: str = dict.get('r')
+        self.s: str = dict.get('s')
+
+
 class Transaction(JSONable):
     def __init__(self, dict):
         self.hash: str = dict.get('hash')
@@ -20,6 +31,10 @@ class Transaction(JSONable):
         self.r: str = dict.get('r')
         self.s: str = dict.get('s')
         self.v: str = dict.get('v')
+        auth_list = get_dict_val(
+            dict, 'authorizationList', 'authorization_list')
+        self.authorization_list: list[Authorization] = [] if auth_list is None else [
+            Authorization(a) for a in auth_list]
 
     @property
     def input(self):
